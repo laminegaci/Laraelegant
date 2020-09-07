9edfa79bbcb472c01b2efbf3241e3e40697783e9
laminegaci <mohamed61lamine@gmail.com>
2020-09-07 15:41:57 +0100

    second commit

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,6 +1,19 @@
 window._ = require('lodash');
 
 /**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+
+    require('bootstrap');
+} catch (e) { }
+
+/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
@@ -26,3 +39,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+//import './jquery-3.2.1.min.js';
+// import './perfect-scrollbar.jquery.min.js';
+// import './waves.js';
+// import './sidebarmenu.js';
+import './custom.min.js';
+// import './dashboard1.js';
