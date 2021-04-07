371344bce0697ded19865891f1f6669107e410e0
laminegaci <mohamed61lamine@gmail.com>
2021-04-07 13:05:17 +0100

    require L.debugbar & mcamara/L.localisation

---
+++ resources/lang/ar/routes.php
@@ -0,0 +1,8 @@
+<?php
+// resources/lang/en/routes.php
+return [
+    "Login"    =>  "دخول",
+    "Register"    =>  "تسجبل",
+    "users" => "المستخدمين",
+    //"article"  =>  "article/{article}",
+];
---
+++ resources/lang/en/routes.php
@@ -0,0 +1,8 @@
+<?php
+// resources/lang/en/routes.php
+return [
+    "Login"    =>  "Login",
+    "Register"    =>  "Register",
+    "users" => "users",
+    //"article"  =>  "article/{article}",
+];
